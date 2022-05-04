b3c47ba1021fe99c4a4d8b474f001248f85e6c01
Saranraj.Nagaiyan <Saranraj.Nagaiyan@CORE-LT-085.coreintegra.in>
2022-05-04 15:52:38 +0530

    called factorial

--- helloworld/src/helloworld/Hello.java
+++ helloworld/src/helloworld/Hello.java
@@ -5,6 +5,8 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Guys !.");
+		Factorial f=new Factorial();
+		System.out.println(f.fact(5));
 	}
 
 }
